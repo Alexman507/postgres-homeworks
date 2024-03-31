a236696ccaada551e9a71b93ce5f7bdaaf54fad2
SRE <Nerocross507@gmail.com>
2024-03-31 22:55:00 +0300

    create tables (customers, employees, orders) and various load data csv's into tables, using PostgreSQL commands

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,30 @@
 -- SQL-команды для создания таблиц
+create database north;
+
+create table customers (
+customer_id varchar(5) not null,
+company_name varchar(100) not null,
+contact_name varchar(100) not null,
+);
+
+create table employees (
+employee_id int primary key auto_increment,
+first_name varchar(100) not null,
+last_name varchar(100) not null,
+title varchar(100) not null,
+birth_date date,
+notes text
+);
+
+create table orders (
+order_id int primary key auto_increment,
+customer_id varchar(5) references customers (customer_id),
+employee_id int references customers (employee_id),
+order_date date not null,
+ship_city varchar(100) not null,
+)
+
+--alternative method for copy data from files.csv
+copy customers from 'C:\Users\SRE\PycharmProjects\postgres-homeworks\homework-1\north_data\customers_data.csv' with (format csv)
+copy employees from 'C:\Users\SRE\PycharmProjects\postgres-homeworks\homework-1\north_data\employees_data.csv' with (format csv)
+copy orders from 'C:\Users\SRE\PycharmProjects\postgres-homeworks\homework-1\north_data\orders_data.csv' with (format csv)
